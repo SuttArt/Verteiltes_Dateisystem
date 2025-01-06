1386725df17b9c5a75cff2223a1eaa3d8fd40349
Artem Suttar <artem.suttar@gmail.com>
2025-01-06 01:01:22 +0100

    Add master-server functionals
    Added randomise crate;
    Not sure if it works;
    TODO: master_server.rs to test it all

--- src/master.rs
+++ src/master.rs
@@ -6,8 +6,12 @@
 
 use std::{
 	net::SocketAddr,
-	sync::{Arc, RwLock}
+	sync::{Arc, RwLock},
+	collections::HashMap
 };
+
+// used for randomise chunk-server
+use rand::seq::SliceRandom;
 
 use tarpc::context::Context;
 
@@ -21,26 +25,54 @@ pub struct GfsMaster(Arc<RwLock<Inner>>);
 /// It contains a list of chunk servers and a registry mapping URLs to their
 /// respective chunk server addresses.
 #[derive(Default)]
-struct Inner;
+struct Inner {
+	// list of chunk servers
+	chunk_servers: Vec<SocketAddr>,
+	// registry mapping URLs to their respective chunk server addresses
+	registry: HashMap<String, SocketAddr>,
+}
 
 // Implementation of the `Master` trait for `GfsMaster`.
 impl Master for GfsMaster {
-	async fn lookup(self, _: Context, _url: String) -> SocketAddr {
-		todo!()
+	async fn lookup(self, _: Context, url: String) -> SocketAddr {
+		// take read lock
+		let inner = self.0.read().unwrap();
+		// find url
+		if let Some(&address) = inner.registry.get(&url) {
+			address
+		} else {
+			// Choose random chunk server if nothing found
+			if let Some(&random_server) = inner.chunk_servers.choose(&mut rand::thread_rng()) {
+				random_server
+			} else {
+				panic!("No chunk servers available for lookup!");
+			}
+		}
 	}
 }
 
 // Implementation of the `ChunkMaster` trait for `GfsMaster`.
 impl ChunkMaster for GfsMaster {
-	async fn register(self, _: Context, _socket_addr: SocketAddr) -> u64 {
-		todo!()
+	async fn register(self, _: Context, socket_addr: SocketAddr) -> u64 {
+		// take write lock
+		let mut inner = self.0.write().unwrap();
+		inner.chunk_servers.push(socket_addr);
+		// Index should be the last one
+		inner.chunk_servers.len() as u64 - 1
 	}
 	
-	async fn insert(self, _: Context, _sender: u64, _url: String) {
-		todo!()
+	async fn insert(self, _: Context, sender: u64, url: String) {
+		// take write lock
+		let mut inner = self.0.write().unwrap();
+		// find address in List
+		if let Some(&address) = inner.chunk_servers.get(sender as usize) {
+			inner.registry.insert(url, address);
+		}
 	}
 	
-	async fn remove(self, _: Context, _url: String) {
-		todo!()
+	async fn remove(self, _: Context, url: String) {
+		// take write lock
+		let mut inner = self.0.write().unwrap();
+		inner.registry.remove(&url);
 	}
 }
